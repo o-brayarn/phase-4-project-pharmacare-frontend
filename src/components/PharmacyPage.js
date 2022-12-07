e3dd1282a16b110f2e08fb2c62a9a6bbb8509b1d
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-07 16:18:30 +0300

    pharmacy page & card initialization

---
+++ src/components/PharmacyPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import NavBar from "./NavBar";
+import PharmacyCard from "./PharmacyCard";
+export default function PharmacyPage() {
+  return (
+    <>
+      {/* <NavBar /> */}
+      <PharmacyCard />
+    </>
+  );
+}
