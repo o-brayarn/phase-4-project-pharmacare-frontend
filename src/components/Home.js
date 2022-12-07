34304edf1d05edb019c86038d1b3f196e837def2
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-07 16:14:58 +0300

    refactor the landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./Register";
 import Login from "./Login";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
@@ -8,28 +8,6 @@ export default function Home() {
   return (
     <Router>
       <div className="Home">
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/login"}>
-              WELCOME TO PHARMACARE
-            </Link>
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/login"}>
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/register"}>
-                    Register
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
         <div className="auth-wrapper">
           <div className="auth-inner">
             <Routes>
