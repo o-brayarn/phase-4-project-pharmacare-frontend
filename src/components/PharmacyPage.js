847bb4546205bc934ef3b38aa03d3ce09ddf0812
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-08 08:55:07 +0300

    get pharmacies

--- src/components/PharmacyPage.js
+++ src/components/PharmacyPage.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import NavBar from "./NavBar";
 import PharmacyCard from "./PharmacyCard";
 export default function PharmacyPage() {
+  const [pharmacies, setPharmacies] = useState([]);
+  useEffect(() => {
+    fetch("/pharmacies")
+      .then((res) => res.json())
+      .then((pharmacy) => setPharmacies(pharmacy));
+  }, []);
+  console.log(pharmacies);
+
   return (
     <>
       {/* <NavBar /> */}
-      <PharmacyCard />
+      {pharmacies.map((pharmacy) => {
+        return <PharmacyCard key={pharmacy.id} pharmacy={pharmacy} />;
+      })}
     </>
   );
 }
