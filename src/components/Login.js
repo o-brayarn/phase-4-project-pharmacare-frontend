17e5faa799df4b2fac27bfd2f4536d1e3303cdb8
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-08 08:55:42 +0300

    refactor login page & modify card

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,77 @@
-import React from "react";
-import LoginForm from "./LoginForm";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-export default function Login() {
-  return <LoginForm />;
+export default function Login({ setUser }) {
+  const nav = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((res) => {
+        if (res.ok) {
+          res.json().then((user) => setUser(user));
+          nav("/pharmacies");
+        }
+      })
+      .catch((e) => {
+        console.log("error Sending Data" + e);
+      });
+  }
+
+  return (
+    <form onChange={setFormData} onSubmit={handleSubmit}>
+      <h3>Login</h3>
+      <div className="mb-3">
+        <label>Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter your email"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <label>Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          required
+        />
+      </div>
+      <div className="mb-3">
+        <div className="custom-control custom-checkbox">
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id="customCheck1"
+          />
+          <label className="custom-control-label" htmlFor="customCheck1">
+            Remember me
+          </label>
+        </div>
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+      <p className="forgot-password text-right">
+        Forgot <a href="reset">password?</a>
+      </p>
+      <p className="forgot-password">
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+    </form>
+  );
 }
--- src/components/PharmacyCard.js
+++ src/components/PharmacyCard.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+// import { Card } from "react-bootstrap";
 
-export default function PharmacyCard() {
+export default function PharmacyCard({ pharmacy }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="container mt-5 mb-3" key={pharmacy.id}>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card p-3 mb-2">
+              <div className="d-flex flex-row align-items-center">
+                <div className="card-body">
+                  <h5 className="card-title">{pharmacy.name}</h5>
+                  <p className="card-text">{pharmacy.location}</p>
+                  <p className="card-text">{pharmacy.address}</p>
+                  <p className="card-text">Email: {pharmacy.email}</p>
+                  <p className="card-text">Call:{pharmacy.phone_number}</p>
+                  <a href="/" className="btn btn-primary">
+                    Check Available Medicine
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
