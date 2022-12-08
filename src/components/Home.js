f25377b512652e6c7b64b2168121d846f8080368
Brayarn <brayarn.oduor@student.moringaschool.com>
2022-12-08 08:57:47 +0300

    add dependencies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Register from "./Register";
-import Login from "./Login";
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
-  return (
-    <Router>
-      <div className="Home">
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Routes>
-              <Route exact path="/" element={<Login />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
-    </Router>
-  );
+  return <>Home</>;
 }
